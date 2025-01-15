0e0646c664f7281eb05573b3507e7f091d30db34
Duyquan935 <duyquan090305@gmail.com>
2025-01-15 12:32:24 +0700

    lab2.2

---
+++ Lab3/App.js
@@ -0,0 +1,16 @@
+import react from "react";
+import {StyleSheet, Text, View } from "react-native";
+export const ViewCoponet = () => {
+return (
+    <View
+    style={{
+        backgroudColor: "Blue",
+        height: 200,
+        width: 200,
+        alignSelf: "center",
+        marginTop: 20,
+    }}
+    ></View>
+);
+};
+export default ViewCoponet;
